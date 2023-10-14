9aa60bc8921a56a56a97dafdd496ab7c8c83ccad
Lathiya Jayesh <lathiyajayesh@Lathiyas-MacBook-Air.local>
2023-10-14 15:48:10 +0530

    comment added

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'src/app.dart';
 import 'src/features/settings/settings_controller.dart';
 import 'src/features/settings/settings_service.dart';
 
+// new branch created : development
+
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
