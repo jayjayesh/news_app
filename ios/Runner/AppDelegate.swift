a13c716461053ea59a1fbe644cbdad7549851601
Lathiya Jayesh <lathiyajayesh77@gmail.com>
2023-10-15 11:41:01 +0530

    pass dara to native host app

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,16 @@
 import UIKit
 import Flutter
 
+private class PigeonApiImplementation: NewsArticleHostApi {
+
+ func sendNewsDetail(newsArticleModel: NewsArticleModelPigeon) {
+  
+    print("\(newsArticleModel.title)")
+    
+  }
+
+}
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +18,11 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    let controller = window?.rootViewController as! FlutterViewController
+    let api = PigeonApiImplementation()
+    NewsArticleHostApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: api)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
