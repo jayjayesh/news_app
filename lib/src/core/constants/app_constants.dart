5affbbd9ba198cdcc4d1a8a2074f584315cc1997
Lathiya Jayesh <lathiyajayesh77@gmail.com>
2023-10-16 17:35:12 +0530

    securing news api key

--- lib/src/core/constants/app_constants.dart
+++ lib/src/core/constants/app_constants.dart
@@ -1,7 +1,7 @@
 class AppConstant {
   static int pageSize = 10;
   // TODO : need to save this key at secure place : into env file
-  static String newsApiKey = 'c1a571d9e5cd4ff3b161d9c7e248bd0f';
+  static String newsApiKey = 'YOUR_NEWS_API_KEY';
   static String newsApiCountry = 'us'; //'in';
   static String placeholderImage =
       'https://placehold.jp/30/969696/ffffff/300x150.png?text=news';
